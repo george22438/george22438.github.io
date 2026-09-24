1a7dac81b015bd7618ea50831c628f2975b5850a
George Garcia <george22438@users.noreply.github.com>
2026-09-24 14:24:54 -0700

    Rename Local nav to Interviews

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* The Garcia Report service worker. Cache version is injected at build time. */
-var VERSION = "20260924211509";
+var VERSION = "20260924212454";
 var PREFIX = "tgr-";
 var PAGES = PREFIX + "pages-" + VERSION;
 var STATIC = PREFIX + "static-" + VERSION;
