b917bb22fdd872779c293aa5bd130e9d1e77d96f
George Garcia <george22438@users.noreply.github.com>
2026-09-24 13:52:40 -0700

    Rankings: replace Zuffa Boxing tab with Boxing (The Ring champion + top 10, all 17 men's divisions)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* The Garcia Report service worker. Cache version is injected at build time. */
-var VERSION = "20260924203144";
+var VERSION = "20260924205216";
 var PREFIX = "tgr-";
 var PAGES = PREFIX + "pages-" + VERSION;
 var STATIC = PREFIX + "static-" + VERSION;
