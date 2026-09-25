77eaf46c98e039ac7f67fa4243f1da0cfebfe260
George Garcia <george22438@users.noreply.github.com>
2026-09-25 14:00:27 -0700

    Refresh Zuffa Boxing 11 hero: dusk London with new gloves
    Slightly darker than the bright daytime composite; regenerated gloves in Tower Bridge scene.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* The Garcia Report service worker. Cache version is injected at build time. */
-var VERSION = "20260925205648";
+var VERSION = "20260925210020";
 var PREFIX = "tgr-";
 var PAGES = PREFIX + "pages-" + VERSION;
 var STATIC = PREFIX + "static-" + VERSION;
