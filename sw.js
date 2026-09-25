2aad97f4d67b47f10a1d5433ad5209387f1fe4e1
George Garcia <george22438@users.noreply.github.com>
2026-09-25 13:36:18 -0700

    Publish Zuffa Boxing 11 BetOnline moneylines + fight-by-fight analysis
    Fisher vs Pirotton London card board (BOL snapshot), boxing category,
    text-free dark sportsbook hero, Hughes/Stephenson omitted as scratched.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* The Garcia Report service worker. Cache version is injected at build time. */
-var VERSION = "20260925170330";
+var VERSION = "20260925203604";
 var PREFIX = "tgr-";
 var PAGES = PREFIX + "pages-" + VERSION;
 var STATIC = PREFIX + "static-" + VERSION;
