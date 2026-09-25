4290fe3b66dc3ca21eb8df13aad010e886d2e2fb
George Garcia <george@thegarciareport.com>
2026-09-25 08:36:21 -0700

    Fantasy: show category board in HTML and cache-bust live JS
    Phone/PWA was likely holding an old fantasy-live.js. Categories are
    now in the static Standings markup too, and scripts use ?v= hashes.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* The Garcia Report service worker. Cache version is injected at build time. */
-var VERSION = "20260925153041";
+var VERSION = "20260925153617";
 var PREFIX = "tgr-";
 var PAGES = PREFIX + "pages-" + VERSION;
 var STATIC = PREFIX + "static-" + VERSION;
