20f87a90db557f98ec2501772d7fa4257d70e39c
George Garcia <george22438@users.noreply.github.com>
2026-09-25 13:57:21 -0700

    Brighten Zuffa Boxing 11 featured thumbnail (London + gloves)
    Replace near-black hero with a much brighter Tower Bridge + red boxing
    gloves composite (1600×900, left-weighted). Bump image cache-bust to
    20260925c. No text/logos. Keeps CNAME, .nojekyll, and post aliases.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* The Garcia Report service worker. Cache version is injected at build time. */
-var VERSION = "20260925204719";
+var VERSION = "20260925205648";
 var PREFIX = "tgr-";
 var PAGES = PREFIX + "pages-" + VERSION;
 var STATIC = PREFIX + "static-" + VERSION;
