3c1b601f7099453a4eb28897e560bd335b42ed3d
George Garcia <george22438@users.noreply.github.com>
2026-09-24 23:51:59 -0700

    Name Fantasy slot 2 Ferm20

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* The Garcia Report service worker. Cache version is injected at build time. */
-var VERSION = "20260925064619";
+var VERSION = "20260925065159";
 var PREFIX = "tgr-";
 var PAGES = PREFIX + "pages-" + VERSION;
 var STATIC = PREFIX + "static-" + VERSION;
